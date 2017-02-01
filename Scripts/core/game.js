909933b45d6ceb517d52fc17401b0cab71ec82ae
Chaosman004 <mr.robert.page@gmail.com>
2017-02-01 10:43:43 -0500

    Added Button Class

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,4 +1,5 @@
 /* core game script */
+/// <reference path="./_reference.ts"/>
 // IIFE - Immediately Invoked Function Expression
 (function () {
     //CreateJS Variables
@@ -6,25 +7,29 @@
     var stage;
     //Game Variables
     var helloLabel;
+    var clickMeButton;
     function Start() {
         canvas = document.getElementById("canvas"); // get ref to canvas element
         stage = new createjs.Stage(canvas); // creating a new stage object
+        stage.enableMouseOver(20);
         createjs.Ticker.framerate = 60; //set fps to 60
         createjs.Ticker.on("tick", Update); // calls the Update function every frame
         Game();
     }
     function Update(event) {
-        helloLabel.rotation += 5; //rotate clockwise 5 degrees every frame
+        /*helloLabel.rotation += 5;//rotate clockwise 5 degrees every frame*/
         stage.update(); //manually redraws the stage
     }
     function Game() {
         console.log("Game Started...");
-        helloLabel = new createjs.Text("Hello, World!", "40px Consolas", "#000000");
-        helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
-        helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
-        helloLabel.x = 160;
-        helloLabel.y = 240;
+        helloLabel = new objects.Label("Hello, World!", "40px Consolas", "#000000", 160, 240, true);
         stage.addChild(helloLabel);
+        //add clickme button
+        clickMeButton = new objects.Button("../../Assets/images/ClickMeButton.png", 160, 300, true);
+        stage.addChild(clickMeButton);
+        clickMeButton.on("click", function (event) {
+            helloLabel.text = "Good bye!";
+        });
     }
     window.onload = Start; //calls the start function when the window finishes loading
 })();
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,4 +1,6 @@
 /* core game script */
+/// <reference path="./_reference.ts"/>
+
 
 // IIFE - Immediately Invoked Function Expression
 
@@ -8,11 +10,13 @@
     let stage: createjs.Stage;
 
     //Game Variables
-    let helloLabel: createjs.Text;
+    let helloLabel: objects.Label;
+    let clickMeButton: objects.Button;
 
     function Start(): void {
         canvas = document.getElementById("canvas"); // get ref to canvas element
         stage = new createjs.Stage(canvas);// creating a new stage object
+        stage.enableMouseOver(20);
         createjs.Ticker.framerate = 60;//set fps to 60
         createjs.Ticker.on("tick", Update);// calls the Update function every frame
 
@@ -21,7 +25,7 @@
 
     function Update(event: createjs.Event): void {
 
-        helloLabel.rotation += 5;//rotate clockwise 5 degrees every frame
+        /*helloLabel.rotation += 5;//rotate clockwise 5 degrees every frame*/
 
         stage.update();//manually redraws the stage
     }
@@ -29,13 +33,16 @@
     function Game(): void {
         console.log("Game Started...");
 
-        helloLabel = new createjs.Text("Hello, World!", "40px Consolas", "#000000");
-        helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
-        helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
-        helloLabel.x = 160;
-        helloLabel.y = 240;
-
+        helloLabel = new objects.Label("Hello, World!", "40px Consolas", "#000000", 160, 240, true);
         stage.addChild(helloLabel);
+
+        //add clickme button
+        clickMeButton = new objects.Button("../../Assets/images/ClickMeButton.png", 160, 300, true)
+        stage.addChild(clickMeButton);
+
+        clickMeButton.on("click", (event: createjs.MouseEvent) => {
+            helloLabel.text = "Good bye!";
+        });
     }
 
     window.onload = Start;//calls the start function when the window finishes loading
