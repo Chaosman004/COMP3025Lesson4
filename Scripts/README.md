5097afc5e36a6ce7aafe8214126804c2d3fbaa17
Chaosman004 <mr.robert.page@gmail.com>
2017-02-01 09:51:02 -0500

    All caught up

--- Scripts/README.md
+++ Scripts/README.md
@@ -1,5 +1,5 @@
 # COMP3025 - W2017 - Lesson 4
 
-## TypeScript Demo with CreaetJS
+## TypeScript Demo with CreateJS
 
 please use **`npm install`** in order to include type definitions as development dependencies for your project.
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -2,9 +2,41 @@
 
 // IIFE - Immediately Invoked Function Expression
 
-(function(){
-console.log("Game Started");
+(function () {
+    //CreateJS Variables
+    let canvas: HTMLElement;
+    let stage: createjs.Stage;
 
+    //Game Variables
+    let helloLabel: createjs.Text;
 
+    function Start(): void {
+        canvas = document.getElementById("canvas"); // get ref to canvas element
+        stage = new createjs.Stage(canvas);// creating a new stage object
+        createjs.Ticker.framerate = 60;//set fps to 60
+        createjs.Ticker.on("tick", Update);// calls the Update function every frame
 
+        Game();
+    }
+
+    function Update(event: createjs.Event): void {
+
+        helloLabel.rotation += 5;//rotate clockwise 5 degrees every frame
+
+        stage.update();//manually redraws the stage
+    }
+
+    function Game(): void {
+        console.log("Game Started...");
+
+        helloLabel = new createjs.Text("Hello, World!", "40px Consolas", "#000000");
+        helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+        helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+        helloLabel.x = 160;
+        helloLabel.y = 240;
+
+        stage.addChild(helloLabel);
+    }
+
+    window.onload = Start;//calls the start function when the window finishes loading
 })();
